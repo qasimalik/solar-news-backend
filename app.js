a84e657bf8e81907add9dab8b063461bc9f60247
qasimalik <qsmmlk12@gmail.com>
2024-01-15 16:47:07 +0530

    cors modification 4

--- app.js
+++ app.js
@@ -23,12 +23,14 @@ const app = express();
 
 // };
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+app.use(cors({ origin: '*' }));
 
 app.use(bodyParser.json({limit: '35mb'}));
 
