857b411ff30b054671ec7faf05fc9b4a3ef866a3
qasimalik <qsmmlk12@gmail.com>
2024-01-15 16:54:36 +0530

    cors modification 5

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
-import express from 'express';
-import{ config } from 'dotenv';
-import cors from 'cors';
-import adminRoutes from './routes/adminRoutes/admin.js';
-import articleRoutes from './routes/articles.js';
-import feedbackRoutes from './routes/feedbacks.js';
-import videoRoutes from './routes/videos.js';
-import bodyParser from 'body-parser';
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import adminRoutes from "./routes/adminRoutes/admin.js";
+import articleRoutes from "./routes/articles.js";
+import feedbackRoutes from "./routes/feedbacks.js";
+import videoRoutes from "./routes/videos.js";
+import bodyParser from "body-parser";
+
 config();
+
 const app = express();
+app.use(express.json());
+const whitelist = [
+  "http://localhost:3000",
+  "https://solar-news-frontend.vercel.app/",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("origin", origin);
+      callback(null, true);
+    } else {
+      console.log("origin", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// const corsPolicy = (req, res, next) => {
-//   const allowedOrigins = "*";
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
+app.use(cors(corsOptions));
 
-//   }
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods","*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   next();
-
-// };
-
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-app.use(cors({ origin: '*' }));
-
-app.use(bodyParser.json({limit: '35mb'}));
+app.use(bodyParser.json({ limit: "35mb" }));
 
 app.use(
   bodyParser.urlencoded({
     extended: true,
-    limit: '35mb',
+    limit: "35mb",
     parameterLimit: 500000,
-  }),
+  })
 );
 
-app.use('/admin', adminRoutes);
-app.use('/articles', articleRoutes);
-app.use('/feedbacks', feedbackRoutes);
-app.use('/videos', videoRoutes);
-app.use('/', (req, res) => {
-  res.send('Welcome to the backend of the website');
+app.use("/admin", adminRoutes);
+app.use("/articles", articleRoutes);
+app.use("/feedbacks", feedbackRoutes);
+app.use("/videos", videoRoutes);
+app.use("/", (req, res) => {
+  res.send("Welcome to the backend of the website");
 });
 
 export default app;
