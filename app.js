4efc3c282de17d0e2007c5d8c7522f0b465ed346
qasimalik <qsmmlk12@gmail.com>
2024-01-15 16:41:57 +0530

    cors modification 2

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ import bodyParser from 'body-parser';
 config();
 const app = express();
 
-const corsPolicy = (req, res, next) => {
-  const allowedOrigins = "*";
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods","*");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
+// const corsPolicy = (req, res, next) => {
+//   const allowedOrigins = "*";
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
 
-};
+//   }
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods","*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
 
-app.use(corsPolicy)
+// };
+
+app.use(cors)
 
 app.use(bodyParser.json({limit: '35mb'}));
 
